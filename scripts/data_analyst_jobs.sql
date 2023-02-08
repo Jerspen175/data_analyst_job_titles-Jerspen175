bd073ccfe8889343008c46070c6c8f453c55116d
Jerod Spence <spencejerod@gmail.com>
2023-02-07 20:09:18 -0600

    Question 9

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -52,12 +52,17 @@
 ---There are 881 unique job titles.
 
 -- 8.	How many unique job titles are there for California companies?
-SELECT DISTINCT(title), location
-FROM data_analyst_jobs
-WHERE location='CA';
+-- SELECT DISTINCT(title), location
+-- FROM data_analyst_jobs
+-- WHERE location='CA';
 ---There are 230 unique job titles in the state CA.
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+SELECT title, AVG(star_rating) AS avg_star_rating, location, review_count
+FROM data_analyst_jobs
+WHERE review_count > '5000'
+GROUP BY title, location, review_count;
+---There are 131 companies with more than 5000 reviews across all locations.
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
